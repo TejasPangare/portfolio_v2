8394bee69beec9685d9bd4dd913d4b06424ed37c
TejasPangare <tejaspangare1004@gmail.com>
2026-06-02 00:24:03 +0530

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio_v2/',
+  base: '/portfolio_v2',
 })
